a2d03fa244fb8cdbd6bca2aee7f2d1d0878875ed
Daniel Ramos <danielmfr6@gmail.com>
2021-04-21 18:51:17 -0300

    Configurar fragment para
    - Configurar lista de dispositivos pareados
    - Configurar lista de dispositivos bluetooth encontrados

--- app/src/main/java/com/daniel/ramos/projetotcc/presenter/adapters/DeviceListPairedAdapter.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/presenter/adapters/DeviceListPairedAdapter.kt
@@ -8,12 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.daniel.ramos.projetotcc.databinding.RowDeviceInfoBinding
-//TODO: Remover da lista dispositivos já pareados
-class DeviceListAdapter(
-    private val context: Context,
-    private val deviceList: MutableList<BluetoothDevice>
-) : RecyclerView.Adapter<DeviceListAdapter.ViewHolder>() {
-    private var _binding: RowDeviceInfoBinding? = null
+import com.daniel.ramos.projetotcc.databinding.RowPairedDeviceInfoBinding
+
+class DeviceListPairedAdapter(private val context: Context, private var deviceList: List<BluetoothDevice>) : RecyclerView.Adapter<DeviceListPairedAdapter.ViewHolder>() {
+    private var _binding: RowPairedDeviceInfoBinding? = null
     private val binding get() = _binding!!
     private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
@@ -23,9 +21,9 @@ class DeviceListAdapter(
         val deviceInfoLayout = binding.linearLayoutDeviceInfo
     }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        _binding = RowDeviceInfoBinding.inflate(LayoutInflater.from(context), parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceListPairedAdapter.ViewHolder {
+        _binding = RowPairedDeviceInfoBinding.inflate(LayoutInflater.from(context), parent, false)
+        deviceList = deviceList.distinct()
         return ViewHolder(binding.root)
     }
 
@@ -33,22 +31,13 @@ class DeviceListAdapter(
         return deviceList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: DeviceListPairedAdapter.ViewHolder, position: Int) {
         val deviceInfoModel = deviceList[position]
         holder.textName.text = deviceInfoModel.name
         holder.textAddress.text = deviceInfoModel.address
         holder.deviceInfoLayout.setOnClickListener {
 //            bluetoothAdapter.cancelDiscovery()
             deviceList[position].createBond()
-        }
-    }
-
-    fun removerItemPorMAC(deviceAddress: String) {
-        deviceList.forEachIndexed { index, bluetoothDevice ->
-            if (deviceAddress == bluetoothDevice.address) {
-                deviceList.removeAt(index)
-                notifyItemRemoved(index)
-            }
         }
     }
 }
--- app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/ConfigurarAppFragment.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/ConfigurarAppFragment.kt
@@ -16,9 +16,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.daniel.ramos.projetotcc.databinding.FragmentConfigurarAppBinding
 import com.daniel.ramos.projetotcc.presenter.ConfigurarAppPresenter
 import com.daniel.ramos.projetotcc.presenter.adapters.DeviceListAdapter
+import com.daniel.ramos.projetotcc.presenter.adapters.DeviceListPairedAdapter
 import com.daniel.ramos.projetotcc.view.activity.MainActivity
 
 
+//TODO: Adicionar progress bar para indicar a busca por dispositivos bluetooth
 class ConfigurarAppFragment : Fragment() {
     private val TAG = "ConfigurarAppFragment"
 
@@ -26,8 +28,13 @@ class ConfigurarAppFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var presenter: ConfigurarAppPresenter
 
+    private lateinit var deviceListPairedAdapter: DeviceListPairedAdapter
+    private lateinit var deviceListAdapter: DeviceListAdapter
+
     private var mDeviceList = arrayListOf<BluetoothDevice>()
+    private var mPairedDeviceList = arrayListOf<BluetoothDevice>()
     lateinit var mBTDevice: BluetoothDevice
+    val btAdapter = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,9 +49,10 @@ class ConfigurarAppFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        val filter = IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
-        MainActivity.context.registerReceiver(broadcastBondStateBT, filter)
+        inicializarBroadcasts()
+
         //TODO: Criar lista de dispositivos disponiveis e dispositivos pareados
+        atualizarDispositivosPareados()
     }
 
     override fun onDestroy() {
@@ -59,33 +67,56 @@ class ConfigurarAppFragment : Fragment() {
 
     private fun configurarBotoes() {
         binding.buscarDispositivos.setOnClickListener {
-            mDeviceList.clear()
-            updateAdapterDispositivosPareados()
+            btAdapter.cancelDiscovery()
             getListDispositivosDisponiveis()
         }
     }
 
     private fun configurarRecyclerView() {
+        deviceListAdapter = DeviceListAdapter(requireContext(), mDeviceList)
+        deviceListPairedAdapter = DeviceListPairedAdapter(requireContext(), mPairedDeviceList)
+        binding.rvDispositivosPareados.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = deviceListPairedAdapter
+        }
         binding.rvDispositivosBlue.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = DeviceListAdapter(context, mDeviceList)
+            adapter = deviceListAdapter
         }
     }
 
+    private fun atualizarDispositivosPareados() {
+        mPairedDeviceList.clear()
+        val pairedDevice: Set<BluetoothDevice> =
+            btAdapter.bondedDevices
+        if (pairedDevice.isNotEmpty()) {
+            for (device in pairedDevice) {
+                mPairedDeviceList.add(device)
+            }
+        }
+        binding.rvDispositivosPareados.recycledViewPool.clear()
+        deviceListPairedAdapter.notifyDataSetChanged()
+    }
+
+    private fun inicializarBroadcasts() {
+        val filterDiscover = IntentFilter().apply {
+            addAction(BluetoothDevice.ACTION_FOUND)
+            addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+            addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        }
+        MainActivity.context.registerReceiver(broadcastDiscoverBTDevices, filterDiscover)
+
+        val filterBondChange = IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
+        MainActivity.context.registerReceiver(broadcastBondStateBT, filterBondChange)
+    }
+
     private fun getListDispositivosDisponiveis() {
-        val btAdapter = BluetoothAdapter.getDefaultAdapter()
-        val filter = IntentFilter()
-
-        filter.addAction(BluetoothDevice.ACTION_FOUND)
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
-
-        MainActivity.context.registerReceiver(broadcastDiscoverBTDevices, filter)
         btAdapter.startDiscovery()
     }
 
-    private fun updateAdapterDispositivosPareados() {
+    private fun updateAdapterDispositivosEncontrados() {
         binding.rvDispositivosBlue.adapter!!.notifyDataSetChanged()
     }
 
@@ -94,6 +125,8 @@ class ConfigurarAppFragment : Fragment() {
             val action = intent.action
             if (BluetoothAdapter.ACTION_DISCOVERY_STARTED == action) {
                 Log.d(TAG, "Action discovery started")
+                mDeviceList.clear()
+                updateAdapterDispositivosEncontrados()
                 //discovery starts, we can show progress dialog or perform other tasks
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
                 Log.d(TAG, "Action discovery Finished")
@@ -105,7 +138,7 @@ class ConfigurarAppFragment : Fragment() {
                     intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
                 if (device != null) {
                     mDeviceList.add(device)
-                    updateAdapterDispositivosPareados()
+                    updateAdapterDispositivosEncontrados()
                 }
             }
         }
@@ -119,7 +152,9 @@ class ConfigurarAppFragment : Fragment() {
                 when (device!!.bondState) {
                     BluetoothDevice.BOND_BONDED -> {
                         Log.d(TAG, "BroadcastReceiver: BOND_BONDED")
-                        mBTDevice = device
+                        mPairedDeviceList.add(device)
+                        atualizarDispositivosPareados()
+                        deviceListAdapter.removerItemPorMAC(device.address)
                     }
                     BluetoothDevice.BOND_BONDING -> {
                         Log.d(TAG, "BroadcastReceiver: BOND_BONDING")
