7214bde39d058748f58e9cd0bf357444a568c79b
Daniel Ramos <danielmfr6@gmail.com>
2021-04-26 21:56:16 -0300

    Criar RealmRepository para ser superclasse dos repositories

---
+++ app/src/main/java/com/daniel/ramos/projetotcc/model/repositories/RealmRepository.kt
@@ -0,0 +1,78 @@
+package com.daniel.ramos.projetotcc.model.repositories
+
+import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.RealmResults
+import io.realm.exceptions.RealmMigrationNeededException
+import java.lang.IllegalArgumentException
+
+open class RealmRepository<T: RealmObject>(var clazz: Class<T>?) {
+
+    lateinit var realm: Realm
+
+    init {
+        init()
+    }
+
+    private fun init() {
+        try {
+            realm = Realm.getDefaultInstance()
+        } catch (e: RealmMigrationNeededException) {
+            e.printStackTrace()
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun salvar(`object`: T) {
+        realm.executeTransaction { realm: Realm ->
+            realm.copyToRealmOrUpdate(
+                `object`
+            )
+        }
+    }
+
+    fun deletarTodasEntidades() {
+        realm.apply {
+            beginTransaction()
+            removeAllChangeListeners()
+            commitTransaction()
+            beginTransaction()
+            deleteAll()
+            commitTransaction()
+        }
+    }
+
+    fun novo(primaryKey: String): T {
+        return if(primaryKey == null) realm.createObject(clazz) else realm.createObject(clazz, primaryKey)
+    }
+
+    fun atualizarObject(atualizarListener: AtualizarListener) {
+        realm.beginTransaction()
+        atualizarListener.atualizar()
+        realm.commitTransaction()
+    }
+
+    fun cancelarTransactions() {
+        if (realm.isInTransaction) realm.cancelTransaction()
+    }
+
+    fun procurarPorId(id: String?): T? {
+        return realm.where(clazz).equalTo("id",id).findFirst()
+    }
+
+    fun deletar(id: String?) {
+        realm.executeTransaction {
+            procurarPorId(id)?.deleteFromRealm()
+        }
+    }
+
+    fun procurarTodos(): RealmResults<T> {
+        return realm.where(clazz).findAll()
+    }
+
+    interface AtualizarListener {
+        fun atualizar()
+    }
+
+}
