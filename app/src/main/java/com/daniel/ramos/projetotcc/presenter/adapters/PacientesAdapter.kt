f476c24b3edb38286e5634e31fd32198f7ddd093
Daniel Ramos <danielmfr6@gmail.com>
2021-04-29 07:37:50 -0300

    Ajustar layout e implementar delete de paciente

--- app/src/main/java/com/daniel/ramos/projetotcc/presenter/adapters/PacientesAdapter.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/presenter/adapters/PacientesAdapter.kt
@@ -1,5 +1,8 @@
 package com.daniel.ramos.projetotcc.presenter.adapters
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,8 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.daniel.ramos.projetotcc.R
 import com.daniel.ramos.projetotcc.databinding.RowPacienteBinding
 import com.daniel.ramos.projetotcc.model.entities.Paciente
+import com.daniel.ramos.projetotcc.model.repositories.PacienteRepository
+import com.daniel.ramos.projetotcc.model.repositories.RealmRepository
 import com.daniel.ramos.projetotcc.presenter.utils.AgeCalculator
 import com.daniel.ramos.projetotcc.view.activity.MainActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.realm.RealmRecyclerViewAdapter
 import io.realm.RealmResults
 
@@ -17,12 +23,15 @@ class PacientesAdapter(private val pacientes: RealmResults<Paciente>, autoUpdate
 
     private var _binding: RowPacienteBinding? = null
     private val binding get() = _binding!!
+    private val pacienteRepository = PacienteRepository()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nome = binding.tvNome
         var sexo = binding.tvSexo
         var idade = binding.tvIdade
+        var delete = binding.ivDelete
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         _binding =
@@ -34,10 +43,37 @@ class PacientesAdapter(private val pacientes: RealmResults<Paciente>, autoUpdate
         val paciente: Paciente? = getItem(position)
         holder.nome.text = paciente!!.nome
         holder.sexo.text = paciente.sexo
-        holder.idade.text = MainActivity.instance!!.getString(R.string.idadePaciente, AgeCalculator.calcularIdadePorData(paciente.dataNascimento))
+        holder.idade.text = MainActivity.instance!!.getString(
+            R.string.idadePaciente,
+            AgeCalculator.calcularIdadePorData(paciente.dataNascimento)
+        )
+        holder.delete.setOnClickListener {
+            openDialogDeletarPaciente(paciente.nome, position)
+        }
     }
 
     override fun getItemCount(): Int {
         return pacientes.size
+    }
+
+    private fun openDialogDeletarPaciente(pacienteNome: String, position: Int) {
+        val dialog: Dialog = MaterialAlertDialogBuilder(MainActivity.instance!!)
+            .setTitle(R.string.deletarPaciente)
+            .setMessage("O paciente $pacienteNome será deletado. Deseja continuar com esta ação?")
+            .setPositiveButton(R.string.confirmar) { dialogInterface, i ->
+                pacienteRepository.atualizarObject(object : RealmRepository.AtualizarListener {
+                    override fun atualizar() {
+                        pacientes.deleteFromRealm(position)
+                    }
+                })
+                notifyItemRemoved(position)
+                dialogInterface.dismiss()
+            }
+            .setNegativeButton(R.string.cancelar) { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }
+            .setCancelable(false)
+            .create()
+        dialog.show()
     }
 }
