2f9e52c1bbf24426684bf3c6f38d6d6d2c2e1c1e
Daniel Ramos <danielmfr6@gmail.com>
2021-04-29 18:04:16 -0300

    Configurar o recyclerView

--- app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/ExerciciosFragment.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/ExerciciosFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.daniel.ramos.projetotcc.R
 import com.daniel.ramos.projetotcc.databinding.FragmentExerciciosBinding
 import com.daniel.ramos.projetotcc.databinding.FragmentRelatoriosBinding
 import com.daniel.ramos.projetotcc.presenter.ExerciciosPresenter
 import com.daniel.ramos.projetotcc.presenter.RelatoriosPresenter
+import com.daniel.ramos.projetotcc.view.activity.MainActivity
 
 class ExerciciosFragment : Fragment() {
     private var _binding: FragmentExerciciosBinding? = null
@@ -22,11 +24,28 @@ class ExerciciosFragment : Fragment() {
     ): View? {
         _binding = FragmentExerciciosBinding.inflate(layoutInflater, container, false)
         inicializarPresenter()
+        configurarRecyclerView()
+        configurarFabAddExercicio()
         return binding.root
     }
 
     private fun inicializarPresenter() {
         presenter = ExerciciosPresenter(this)
+    }
+
+    private fun configurarRecyclerView() {
+        binding.rvExercicios.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(MainActivity.instance)
+            //TODO: Configurar adapter
+//            adapter = presenter.getAdapterExercicios()
+        }
+    }
+
+    private fun configurarFabAddExercicio() {
+        binding.fabAddExercicio.setOnClickListener {
+            //TODO: Configurar para abrir um novo fragment para adicionar as configurações e salva-las
+        }
     }
 
 }
