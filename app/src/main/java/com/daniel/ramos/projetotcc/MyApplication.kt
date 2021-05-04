635cea81e50e1e5269d59a2a3f49004d1a0907bd
Daniel Ramos <danielmfr6@gmail.com>
2021-05-04 08:28:29 -0300

    Configurar conexão BluetoothServiceA

--- app/src/main/java/com/daniel/ramos/projetotcc/MyApplication.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/MyApplication.kt
@@ -1,28 +1,17 @@
 package com.daniel.ramos.projetotcc
 
 import android.app.Application
-import com.daniel.ramos.projetotcc.presenter.BlueComm
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
 class MyApplication : Application() {
 
-    lateinit var myBlueComm: BlueComm
-
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        inicializarBluetoothComm()
         //TODO: Configurar realm ou Room
         //TODO: Inicializar firebase
     }
-
-    // Desta forma eu crio uma instância da comunicação bluetooth que não será destruida em uma fragmento/activity
-    // Como é um objeto público bastara acessar seus metodos recuperando o applicationContenxt
-    private fun inicializarBluetoothComm() {
-        myBlueComm = BlueComm()
-    }
-
 
     companion object {
         fun configurarRealm() {
