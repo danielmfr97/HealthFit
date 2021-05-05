857ce008962d18374b3c401e02cdf98b3d8c16a6
Daniel Ramos <danielmfr6@gmail.com>
2021-05-04 22:43:21 -0300

    Adicionar lista de cores para seleção

---
+++ app/src/main/java/com/daniel/ramos/projetotcc/presenter/enums/Cores.kt
@@ -0,0 +1,9 @@
+package com.daniel.ramos.projetotcc.presenter.enums
+
+import com.daniel.ramos.projetotcc.R
+
+enum class Cores(val nome: String, val rgb: Int) {
+    VERMELHO("Vermelho", R.color.vermelho),
+    VERDE("Verde", R.color.verde),
+    AZUL("Azul", R.color.azul)
+}
