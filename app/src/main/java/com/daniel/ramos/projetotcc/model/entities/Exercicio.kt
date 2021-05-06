189357be3ee30dcf6d20673c94562ab782b671f5
Daniel Ramos <danielmfr6@gmail.com>
2021-05-05 21:18:04 -0300

    Remover cores e alterar atributos do Exercicio

--- app/src/main/java/com/daniel/ramos/projetotcc/model/entities/Exercicio.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/model/entities/Exercicio.kt
@@ -3,20 +3,29 @@ package com.daniel.ramos.projetotcc.model.entities
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
+// JSON sempre enviado, mesmo quando parar o exercicio.
 open class Exercicio : RealmObject() {
     @PrimaryKey
     var id: String = ""
-    // Random, Sequence
-    var tipoExericicio = ""
-    // Numero de dispositivos usados para atividade
-    var dispositivos: String = ""
-    var cor: String = ""
-    // Especifica se a luz desligará quando houver hit, timeout ou ambos
-    var desligarLuz = ""
-    // Tempo para o proximo dispositivo ligar a luz
-    var delayLuzTempo: String = ""
+
     // TEmpo que o exercicio irá duras
-    var exercicioDuracao: String = ""
-    // Numero de vezes que o exercicio se repete
-    var ciclosExercicio: String = ""
+    var exercicioDuracao: Long? = null // milli seconds
+
+    // True or False se terá timeout
+    var timeOutSensor: Boolean? = null //
+
+    // Tempo para o dispostiivo desligar a luz
+    var timeout: Long? = null // ms, caso timeOutSensor false -> return 0
+
+    // Tempo que o usuario conta erro, ou seja se demorar mt tempo com pé em cima so conta erro dps
+    var delayContarErro: Long? = null // threshold para erro do usuario
+
+    //
+    var start: Boolean = false // Setado para true quando quiser iniciar o exercicio
+
+    // Numero de dispositivos usados para atividade
+    var sensoresUsados: Int? = null // Se enviar 4 reduza um para enviar o dado no json
+
+    // A priori será sempre RANDOM
+    var tipoExericicio: String? = null // Tipo de exercicio
 }
