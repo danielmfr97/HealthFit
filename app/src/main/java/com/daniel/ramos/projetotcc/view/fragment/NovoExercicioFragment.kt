47f574cb6c6233cb25cd8fdbf4d20ea18839eb49
Daniel Ramos <danielmfr6@gmail.com>
2021-05-09 18:38:34 -0300

    Adicionar elementos de layout

--- app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/NovoExercicioFragment.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/view/fragment/NovoExercicioFragment.kt
@@ -1,20 +1,14 @@
 package com.daniel.ramos.projetotcc.view.fragment
 
 import android.os.Bundle
-import android.util.Log
-import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AutoCompleteTextView
-import com.daniel.ramos.projetotcc.R
-import com.daniel.ramos.projetotcc.databinding.FragmentMeusPacientesBinding
 import com.daniel.ramos.projetotcc.databinding.FragmentNovoExercicioBinding
-import com.daniel.ramos.projetotcc.presenter.MeusPacientesPresenter
 import com.daniel.ramos.projetotcc.presenter.NovoExercicioPresenter
-import com.daniel.ramos.projetotcc.presenter.enums.Cores
 import com.daniel.ramos.projetotcc.presenter.enums.TipoExercicio
 
 class NovoExercicioFragment : Fragment() {
@@ -29,7 +23,6 @@ class NovoExercicioFragment : Fragment() {
         _binding = FragmentNovoExercicioBinding.inflate(layoutInflater, container, false)
         inicializarPresenter()
         configurarTipoExercicio()
-//        configurarSelecaoCor()
         return binding.root
     }
 
@@ -45,19 +38,7 @@ class NovoExercicioFragment : Fragment() {
         }
     }
 
-//    private fun configurarSelecaoCor() {
-//        registerForContextMenu((binding.menuSelecionarCor.editText as? AutoCompleteTextView)!!)
-//        (binding.menuSelecionarCor.editText as? AutoCompleteTextView)?.apply {
-//            setAdapter(presenter.getAdapterSelecionarCor())
-//            onItemClickListener = onCorSelecionada
-//        }
-//    }
-
     private val onExercicioSelecionado =
         AdapterView.OnItemClickListener { _, _, position, _ -> binding.menuTipoExercicio.helperText = TipoExercicio.values()[position].descricao }
 
-//    private val onCorSelecionada =
-//        AdapterView.OnItemClickListener { _, _, position, _ ->
-//            binding.menuSelecionarCor.helperText = "O dispositivo utilizará a cor ${Cores.values()[position].nome} para sinalização"
-//        }
 }
