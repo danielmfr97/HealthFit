ca5d98f291c6052e971ef9ffa241fe02f6da7165
Daniel Ramos <danielmfr6@gmail.com>
2021-05-10 06:41:22 -0300

    Adicionar ExercicioModel ao ModelFactory.kt

--- app/src/main/java/com/daniel/ramos/projetotcc/model/factories/ModelFactory.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/model/factories/ModelFactory.kt
@@ -1,13 +1,21 @@
 package com.daniel.ramos.projetotcc.model.factories
 
+import com.daniel.ramos.projetotcc.model.ExercicioModel
 import com.daniel.ramos.projetotcc.model.PacienteModel
 
 object ModelFactory {
     private var pacienteModel: PacienteModel? = null
+    private var exercicioModel: ExercicioModel? = null
 
     val getPacienteModel: PacienteModel
         get() {
             if (pacienteModel == null) pacienteModel = PacienteModel()
             return pacienteModel as PacienteModel
         }
+
+    val getExercicioModel: ExercicioModel
+    get() {
+        if (exercicioModel == null) exercicioModel = ExercicioModel()
+        return exercicioModel as ExercicioModel
+    }
 }
