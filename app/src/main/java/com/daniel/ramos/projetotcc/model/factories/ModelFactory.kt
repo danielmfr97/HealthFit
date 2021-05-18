218ee0ff8d9d9f97b5ca8ab8f72374f8d1216bad
Daniel Ramos <danielmfr6@gmail.com>
2021-05-18 07:54:25 -0300

    Alterar ModelFactory

--- app/src/main/java/com/daniel/ramos/projetotcc/model/factories/ModelFactory.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/model/factories/ModelFactory.kt
@@ -3,11 +3,13 @@ package com.daniel.ramos.projetotcc.model.factories
 import com.daniel.ramos.projetotcc.model.ExercicioModel
 import com.daniel.ramos.projetotcc.model.PacienteModel
 import com.daniel.ramos.projetotcc.model.ResultadoModel
+import com.daniel.ramos.projetotcc.presenter.BluetoothServiceA
 
 object ModelFactory {
     private var pacienteModel: PacienteModel? = null
     private var exercicioModel: ExercicioModel? = null
     private var resultadoModel: ResultadoModel? = null
+    private var bluetoothServiceA: BluetoothServiceA? = null
 
     val getPacienteModel: PacienteModel
         get() {
@@ -26,4 +28,11 @@ object ModelFactory {
         if (resultadoModel == null) resultadoModel = ResultadoModel()
         return resultadoModel as ResultadoModel
     }
+
+    val getBluetoothServiceA: BluetoothServiceA
+        get() {
+            if (ModelFactory.bluetoothServiceA == null)
+                ModelFactory.bluetoothServiceA = BluetoothServiceA()
+            return ModelFactory.bluetoothServiceA as BluetoothServiceA
+        }
 }
