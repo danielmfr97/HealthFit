d77e770f844236d6c39c0f31f97b1d0a79c8e017
Daniel Farias <danielmfr6@gmail.com>
2021-07-12 09:54:42 -0300

    Alterar listeners

--- app/src/main/java/com/daniel/ramos/projetotcc/presenter/MeusPacientesPresenter.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/presenter/MeusPacientesPresenter.kt
@@ -4,6 +4,7 @@ import com.daniel.ramos.projetotcc.model.entities.Paciente
 import com.daniel.ramos.projetotcc.model.factories.ModelFactory
 import com.daniel.ramos.projetotcc.presenter.adapters.PacientesAdapter
 import com.daniel.ramos.projetotcc.presenter.dialogs.DialogAddPaciente
+import com.daniel.ramos.projetotcc.presenter.listeners.OnPacienteAdicionado
 import com.daniel.ramos.projetotcc.view.fragment.MeusPacientesFragment
 
 class MeusPacientesPresenter(private val view: MeusPacientesFragment) {
