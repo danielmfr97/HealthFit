1deb631dd1e9451f61c86989456d1085048b59f3
Daniel Ramos <danielmfr6@gmail.com>
2021-09-26 23:18:50 -0300

    Atualizar UI para usuário identificar buscar de devices

--- app/src/main/java/com/daniel/ramos/projetotcc/presenter/ConfigurarAppPresenter.kt
+++ app/src/main/java/com/daniel/ramos/projetotcc/presenter/ConfigurarAppPresenter.kt
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.daniel.ramos.projetotcc.MyApplication
 import com.daniel.ramos.projetotcc.view.activity.MainActivity
 import com.daniel.ramos.projetotcc.view.fragment.ConfigurarAppFragment
 
@@ -50,11 +49,15 @@ class ConfigurarAppPresenter(private val view: ConfigurarAppFragment?) {
             val action = intent.action
             if (BluetoothAdapter.ACTION_DISCOVERY_STARTED == action) {
                 Log.d(TAG, "Action discovery started")
+                view?.enableDisableProgress(true)
                 view?.mDeviceList?.clear()
                 view?.updateAdapterDispositivosEncontrados()
                 //discovery starts, we can show progress dialog or perform other tasks
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
                 Log.d(TAG, "Action discovery Finished")
+                view?.enableDisableProgress(false)
+                if (view?.mDeviceList.isNullOrEmpty())
+                    MainActivity.openToastShort("Nenhum dispositivo encontrado. Tente novamente.")
                 //discovery finishes, dismis progress dialog
             } else if (BluetoothDevice.ACTION_FOUND == action) {
                 Log.d(TAG, "Bluetooth encontrado")
